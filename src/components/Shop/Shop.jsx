74eda21ed00517085603f0390d6dd7ee784dd8d2
Masum Rahman <masumrahman73@gmail.com>
2023-03-30 01:38:46 +0600

    added comment for handleAddToCart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -43,11 +43,13 @@ const Shop = () => {
         //cart.push(product); JS style
         //const newCart = [...cart, product];
         let newCart = [];
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist update quantity by 1
         const exists = cart.find(pd => pd.id === product.id);
-        if(!exists){
+        if (!exists) {
             product.quantity = 1;
-            newCart = [...cart,product]
-        }else{
+            newCart = [...cart, product]
+        } else {
             exists.quantity = exists.quantity + 1;
             const remaining = cart.filter(pd => pd.id !== product.id);
             newCart = [...remaining, exists];
