616eb30d1a9f152acc40530a7987cfb53d99bd17
Masum Rahman <masumrahman73@gmail.com>
2023-05-20 15:01:27 +0600

    code refactor with backend url json

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import './ReviewItem.css'
 const ReviewItem = ({product, handleRemoveFromCart}) => {
-    const { id, img, price, name, quantity } = product;
+    const { _id, img, price, name, quantity } = product;
     return (
         <div className='review-item'>
             <img src={img} alt="" />
@@ -12,7 +12,7 @@ const ReviewItem = ({product, handleRemoveFromCart}) => {
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
             </div>
-            <button onClick={() => handleRemoveFromCart(id)} className='btn-delete'><FontAwesomeIcon className='delete-icon' icon={faTrashAlt} /></button>
+            <button onClick={() => handleRemoveFromCart(_id)} className='btn-delete'><FontAwesomeIcon className='delete-icon' icon={faTrashAlt} /></button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -10,7 +10,7 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data));
 
@@ -24,7 +24,7 @@ const Shop = () => {
         for (const id in storedCart) {
             //console.log(id);
             // step 2: get the product by using id
-            const addedProduct = products.find(product => product.id === id);
+            const addedProduct = products.find(product => product._id === id);
             ///console.log(addedProduct);
             if (addedProduct) {
                 // step 3: get quantity of the product
@@ -47,17 +47,17 @@ const Shop = () => {
         let newCart = [];
         // if product doesn't exist in the cart, then set quantity = 1
         // if exist update quantity by 1
-        const exists = cart.find(pd => pd.id === product.id);
+        const exists = cart.find(pd => pd._id === product._id);
         if (!exists) {
             product.quantity = 1;
             newCart = [...cart, product]
         } else {
             exists.quantity = exists.quantity + 1;
-            const remaining = cart.filter(pd => pd.id !== product.id);
+            const remaining = cart.filter(pd => pd._id !== product._id);
             newCart = [...remaining, exists];
         }
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(product._id);
     }
     const handleClearCart = () => {
         setCart([]);
@@ -69,7 +69,7 @@ const Shop = () => {
             <div className="products-container">
                 {
                     products.map(product => <Product
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                     ></Product>)
